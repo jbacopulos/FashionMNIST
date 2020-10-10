7c54704186bb301369464ad7d689d078f1131969
jbacopulos <johnbacopulos@gmail.com>
2020-10-10 11:52:51 -0400

    Update images and model

--- train_model.py
+++ train_model.py
@@ -20,6 +20,7 @@ test_images = test_images / 255
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(128, activation='relu'),
     keras.layers.Dense(10)
 ])
 
